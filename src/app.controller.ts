efaa6172ac0febb4f257ae1cdfd4d2dbb768c47a
chanweiyan <chanweiyan007@gmail.com>
2026-05-07 21:01:25 +0800

    fix: implement LoginGuard and test access_token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LoginGuard } from './login.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('require-login')
+  @UseGuards(LoginGuard)
+  getProtected(): string {
+    return 'This is a protected route';
+  }
+
+  @Get('not-require-login')
+  getUnprotected(): string {
+    return 'This is an unprotected route';
   }
 }
---
+++ src/login.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LoginGuard implements CanActivate {
+  @Inject(JwtService)
+  private readonly jwtService: JwtService;
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+    if (!authHeader) {
+      throw new UnauthorizedException('Authorization header is missing');
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Token is missing');
+    }
+
+    try {
+      const decoded = this.jwtService.verify(token);
+      console.log('Decoded token:', decoded);
+      request['user'] = decoded;
+      return true;
+    } catch (err) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+}
