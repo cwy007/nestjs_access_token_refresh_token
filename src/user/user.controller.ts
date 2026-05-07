b6af2fc517be50247bf16ada88eaf0f1ce2f5b3e
chanweiyan <chanweiyan007@gmail.com>
2026-05-07 20:43:34 +0800

    fix: implement /user/register & /user/login api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,33 +2,21 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @Post('login')
+  async login(@Body() userLoginDto: UserLoginDto) {
+    const user = await this.userService.login(userLoginDto);
+    return user;
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Post('register')
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.register(createUserDto);
+    return user;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  @InjectEntityManager()
+  private readonly entityManager: EntityManager;
+
+  async login(userLoginDto: UserLoginDto) {
+    const user = await this.entityManager.findOne(User, {
+      where: {
+        username: userLoginDto.username,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('用户不存在', 404);
+    }
+
+    if (user.password !== userLoginDto.password) {
+      throw new HttpException('密码错误', 400);
+    }
+
+    return user;
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
+  async register(createUserDto: CreateUserDto) {
+    const existingUser = await this.entityManager.findOne(User, {
+      where: {
+        username: createUserDto.username,
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    if (existingUser) {
+      throw new HttpException('用户名已存在', 400);
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    const user = new User();
+    user.username = createUserDto.username;
+    user.password = createUserDto.password;
+    await this.entityManager.save(User, user);
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return user;
   }
 }
